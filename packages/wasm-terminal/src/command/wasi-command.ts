89235fc7d099a43fac05e0fc70d430964ca3afdf
Jim Pick <jim@jimpick.com>
2021-03-31 18:52:48 -0700

    Working wasm terminal with go!

--- packages/wasm-terminal/src/command/wasi-command.ts
+++ packages/wasm-terminal/src/command/wasi-command.ts
@@ -49,89 +49,62 @@ export default class WASICommand extends Command {
       let wasmModuleProxy = await WebAssembly.compile(
         await response.arrayBuffer()
       )
-      importsProxy = wasi.getImports(wasmModuleProxy)
-      go = new (window as any).Go()
+      const getImports = wasi.getImports.bind(wasiProxy)
+      importsProxy = getImports(wasmModuleProxy)
+      go = new (window as any).Go({
+        wasiProxy: wasiProxy,
+        wasiProxyImports:
+          importsProxy.wasi_snapshot_preview1 || importsProxy.wasi_unstable
+      })
       imports = go.importObject
     }
 
-    /*
-    fd_write: wrap(
-        (fd: number, iovs: number, iovsLen: number, nwritten: number) => {
-          const stats = CHECK_FD(fd, WASI_RIGHT_FD_WRITE);
-          let written = 0;
-          getiovs(iovs, iovsLen).forEach(iov => {
-            let w = 0;
-            while (w < iov.byteLength) {
-              const i = fs.writeSync(
-                stats.real,
-                iov,
-                w,
-                iov.byteLength - w,
-                stats.offset ? Number(stats.offset) : null
-              );
-              if (stats.offset) stats.offset += BigInt(i);
-              w += i;
-            }
-            written += w;
-          });
-          this.view.setUint32(nwritten, written, true);
-          return WASI_ESUCCESS;
-        }
-      ),
-      */
+    if (!imports.go) {
+      const getiovs = (iovs: number, iovsLen: number) => {
+        wasi.refreshMemory()
+        const buffers = Array.from({ length: iovsLen }, (_, i) => {
+          const ptr = iovs + i * 8
+          const buf = wasi.view.getUint32(ptr, true)
+          const bufLen = wasi.view.getUint32(ptr + 4, true)
+          return new Uint8Array(wasi.memory.buffer, buf, bufLen)
+        })
+        return buffers
+      }
 
-    const getiovs = (iovs: number, iovsLen: number) => {
-      // iovs* -> [iov, iov, ...]
-      // __wasi_ciovec_t {
-      //   void* buf,
-      //   size_t buf_len,
-      // }
-
-      wasi.refreshMemory()
-
-      const buffers = Array.from({ length: iovsLen }, (_, i) => {
-        const ptr = iovs + i * 8
-        const buf = wasi.view.getUint32(ptr, true)
-        const bufLen = wasi.view.getUint32(ptr + 4, true)
-        return new Uint8Array(wasi.memory.buffer, buf, bufLen)
-      })
-
-      return buffers
-    }
-
-    const importsWasi = imports.wasi_snapshot_preview1 || imports.wasi_unstable
-    const importsProxyWasi =
-      importsProxy.wasi_snapshot_preview1 || importsProxy.wasi_unstable
-    const old_fd_write = importsWasi.fd_write
-    importsWasi.fd_write = function (
-      fd: number,
-      iovs: number,
-      iovsLen: number,
-      nwritten: number
-    ) {
-      console.log('Jim fd_write', fd, iovs, iovsLen, nwritten)
-      wasiProxy.refreshMemory()
-      const proxyIovs = iovs // 0
-      const newIovs = 0
-      let newBufPtr = 8 * iovsLen
-      getiovs(iovs, iovsLen).forEach((iov, i) => {
-        const utf8decoder = new TextDecoder()
-        console.log('Jim iov', utf8decoder.decode(new Uint8Array(iov)))
-        // Copy iov to proxy memory
-        const ptr = iovs + i * 8
-        const buf = wasi.view.getUint32(ptr, true)
-        const bufLen = wasi.view.getUint32(ptr + 4, true)
-        const newPtr = newIovs + i * 8
-        wasiProxy.view.setUint32(newPtr, newBufPtr, true)
-        wasiProxy.view.setUint32(newPtr + 4, bufLen, true)
-        // FIXME: Copy bytes in
-        const dest = new Uint8Array(wasiProxy.memory.buffer, newBufPtr, bufLen)
-        newBufPtr += bufLen
-        dest.set(iov)
-      })
-      const newNwritten = newBufPtr
-      old_fd_write(fd, iovs, iovsLen, nwritten)
-      importsProxyWasi.fd_write(fd, newIovs, iovsLen, newNwritten)
+      const wasiImports = imports.wasi_snapshot_preview1 || imports.wasi_unstable
+      const wasiProxyImports =
+        importsProxy.wasi_snapshot_preview1 || importsProxy.wasi_unstable
+      const old_fd_write = wasiImports.fd_write
+      wasiImports.fd_write = function (
+        fd: number,
+        iovs: number,
+        iovsLen: number,
+        nwritten: number
+      ) {
+        console.log('Jim fd_write', fd, iovs, iovsLen, nwritten)
+        wasiProxy.refreshMemory()
+        const proxyIovs = iovs // 0
+        const newIovs = 0
+        let newBufPtr = 8 * iovsLen
+        getiovs(iovs, iovsLen).forEach((iov, i) => {
+          const utf8decoder = new TextDecoder()
+          console.log('Jim iov', utf8decoder.decode(new Uint8Array(iov)))
+          // Copy iov to proxy memory
+          const ptr = iovs + i * 8
+          const buf = wasi.view.getUint32(ptr, true)
+          const bufLen = wasi.view.getUint32(ptr + 4, true)
+          const newPtr = newIovs + i * 8
+          wasiProxy.view.setUint32(newPtr, newBufPtr, true)
+          wasiProxy.view.setUint32(newPtr + 4, bufLen, true)
+          // FIXME: Copy bytes in
+          const dest = new Uint8Array(wasiProxy.memory.buffer, newBufPtr, bufLen)
+          newBufPtr += bufLen
+          dest.set(iov)
+        })
+        const newNwritten = newBufPtr
+        old_fd_write(fd, iovs, iovsLen, nwritten)
+        wasiProxyImports.fd_write(fd, newIovs, iovsLen, newNwritten)
+      }
     }
 
     console.log('Jim imports', imports)
